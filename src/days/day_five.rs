98fa2ad62381c4f2aa778f6d3d82c41e836bd493
Kaidan <fishstacks@protonmail.com>
2017-12-08 03:29:00 +1100

    Completed day 5 part 1

---
+++ src/days/day_five.rs
@@ -0,0 +1,20 @@
+use super::helper;
+
+pub fn part_one() -> u32 {
+    let mut input: Vec<i32> = helper::input::into_string("./days/day_five.txt")
+        .split('\n')
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect();
+    let mut steps: u32 = 0;
+    let mut current_index: i32 = 0;
+    loop {
+        if let Some(instruction) = input.clone().get(current_index as usize) {
+            steps += 1;
+            input[current_index as usize] += 1;
+            current_index += instruction;
+        } else {
+            break;
+        }
+    }
+    steps
+}
